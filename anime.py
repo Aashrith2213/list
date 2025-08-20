83669270f84cafde3e0f01c2f44674726d4ffefb
Aashrith2213 <aashrith1530@gmail.com>
2025-08-20 10:55:45 +0530

    Add files via upload

---
+++ anime.py
@@ -0,0 +1,9 @@
+li=['luffy','ichigo','zaraki','goku','hinata']
+search_word="hinata"
+replace_word="aizen"
+li.insert(2,"eren")
+for i in range(len(li)):
+   if li[i]==search_word:
+    li[i]=replace_word
+    li[i]==insert_word
+print(li)
